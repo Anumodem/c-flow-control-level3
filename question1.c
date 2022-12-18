74610ed664d9cf93fecc2d2b74a53b1dcf290382
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 21:36:20 +0530

    c

---
+++ question1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int L,D;
+scanf("%d %d",&L,&D);
+int sec=D/0.5;
+int song=sec/L+1;
+if(song!=sec){
+printf("%d",song);
+}
+else{
+ printf("%d",song);
+}
+return 0; }
