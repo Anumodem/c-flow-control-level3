32c815fafa507db4265169e9273df42d1ac4b09e
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 21:40:32 +0530

    c

---
+++ question7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int travelmode;
+    scanf("%d",&travelmode);
+    switch(travelmode)
+    {
+        case 1 :
+        printf("Car is booked");
+        break;
+        case 2 :
+        printf("Bus is booked");
+        break;
+        case 3 :
+        printf("Flight is booked");
+        break;
+        default :
+        printf("Invalid Request");
+        break;
+    }
+     return 0;
+}
